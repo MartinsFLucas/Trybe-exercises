492a56c9298c4a373db8017cd7ceedfe96858eb5
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-09 15:46:23 -0300

    Update the README file content

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts3.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts3.js
@@ -1,4 +1,8 @@
 const score = 59;
+//const score = 81;
+//const score = 65;
+//you can change the score value to verify that the if/else structure works (remember to coment the previous score value to prevent errors)
+
 
 if (score > 80) {
   console.log("Parabéns, você foi aprovado(o)!");
