a3ac2096fb59af17b6d96b27e365c0db36454b3b
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-09 16:09:29 -0300

    Print the assigned message on the screen

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts4.js
@@ -1,10 +1,10 @@
-const currentHour = 6;
+const currentHour = 22;
 let message = "";
 
 if (currentHour >= 22) {
     message = "Não deveríamos comer nada, é hora de dormir";
 } else if (currentHour >= 18 && currentHour < 22) {
-    message = "Rango da noite, vamos jantas :D";
+    message = "Rango da noite, vamos jantar :D";
 } else if (currentHour >= 14 && currentHour < 18) {
     message = "Vamos fazer um bolo pro café da tarde?";
 } else if (currentHour >= 11 && currentHour < 14) {
@@ -12,3 +12,5 @@ if (currentHour >= 22) {
 } else if (currentHour >= 4 && currentHour < 11) {
     message = "Hmmm, cheiro de café recém passado";
 }
+
+console.log(message);
