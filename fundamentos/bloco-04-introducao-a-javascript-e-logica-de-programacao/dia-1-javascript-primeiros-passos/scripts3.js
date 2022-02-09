549c47e13aa5f7e291287d2aa384257b887e52bc
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-09 15:39:31 -0300

    Implement an if/else to print a different message for each result

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts3.js
@@ -0,0 +1,9 @@
+const score = 59;
+
+if (score > 80) {
+  console.log("Parabéns, você foi aprovado(o)!");
+} else if (score < 60) {
+  console.log("Você foi reprovado!");
+} else {
+  console.log("Você está na nossa lista de espera");
+}
