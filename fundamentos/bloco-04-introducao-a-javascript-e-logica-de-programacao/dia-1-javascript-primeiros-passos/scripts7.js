c6cd9e6df6c8c9f5a1b1c180df02656a08947b6b
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-14 16:33:03 -0300

    Create a script to show basic arithmetic operations

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts7.js
@@ -0,0 +1,19 @@
+// Exercício 01
+let a = 4;
+let b = 2;
+
+// Addition
+console.log(a+b);
+
+// Subtraction
+console.log(a-b);
+
+// Multiplication
+console.log(a*b);
+
+// Division
+console.log(a/b);
+
+// Modulus
+console.log(a%b);
+
