3c748e5a032b93827cf9e36a1c2e1b05cd20d935
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-15 18:48:38 -0300

    Adiciona o elemento 'contato' no final do array menu

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
@@ -1,13 +1,22 @@
-// Exercício 1
+// -----------------Exercício 1-----------------
 
 /* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
  */
-//
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// -----------------Exercício 2-----------------
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu.indexOf('Portfólio');
 
-console.log(indexOfPortfolio);
+console.log(indexOfPortfolio); */
+
+// -----------------Exercício 3-----------------
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push('Contato');
+
+console.log(menu);
