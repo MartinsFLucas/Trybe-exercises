b8d67e2ac30cc4ad1c14a104f66da04846af784c
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-17 18:51:24 -0300

    Utiliza o console.log dentro de um for para imprimir todos os valores de um array

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
@@ -31,8 +31,16 @@ for (let index = 0; index < groceryList.length; index += 1) {
 
 // -----------------Exercício 5-----------------
 
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+/* let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 
 for (let element of names) {
   console.log(element);
+} */
+
+// -----------------Exercício 6-----------------
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
 }
