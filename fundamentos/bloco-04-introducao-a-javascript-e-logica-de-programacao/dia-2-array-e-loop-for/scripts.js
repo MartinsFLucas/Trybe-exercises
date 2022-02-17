40f7b9d3ec6a8b5bf18a2cc42d8a4dcae79e6c0a
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-17 19:05:48 -0300

    Verifica se o valor retornado no codigo anterior e maior ou igual a 20

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
@@ -73,4 +73,17 @@ console.log(numbersMean); */
 // -----------------Exercício 9-----------------
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let numbersMean;
 
+for (let index = 0; index < numbers.length; index += 1) {
+  sum += numbers[index];
+}
+
+numbersMean = sum / numbers.length;
+
+if (numbersMean > 20) {
+  console.log('Valor maior que 20');
+} else {
+  console.log('Valor menor ou igual a 20');
+}
