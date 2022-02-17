6c6fc381090a161c045c601e916c4a2da23bfa73
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-17 19:18:04 -0300

    Conta quantos numeros impares existem no array

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
@@ -90,7 +90,7 @@ if (numbersMean > 20) {
 
 // -----------------Exercício 10-----------------
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let largest = numbers[0];
 
 for (let index = 1; index < numbers.length; index += 1) {
@@ -99,4 +99,20 @@ for (let index = 1; index < numbers.length; index += 1) {
   }
 }
 
-console.log(largest);
+console.log(largest); */
+
+// -----------------Exercício 11-----------------
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let countOdd = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2) {
+    countOdd += 1;
+  }
+}
+
+console.log(countOdd); */
+
+// -----------------Exercício 12-----------------
+
