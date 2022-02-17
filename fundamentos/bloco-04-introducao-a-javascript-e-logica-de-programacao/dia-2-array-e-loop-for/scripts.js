dc5a9c16f2e5a8fb38f0c460602306d47e3a0074
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-17 19:02:23 -0300

    Calcula a media aritmetica dos valores contidos no array

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
@@ -47,11 +47,30 @@ for (let index = 0; index < numbers.length; index += 1) {
 
 // -----------------Exercício 7-----------------
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
   sum += numbers[index];
 }
 
-console.log(sum);
+console.log(sum); */
+
+// -----------------Exercício 8-----------------
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let numbersMean;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  sum += numbers[index];
+}
+
+numbersMean = sum / numbers.length;
+
+console.log(numbersMean); */
+
+// -----------------Exercício 9-----------------
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
