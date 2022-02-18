6ceebd1739324754d9a549f4bcc4b9a970674118
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-18 19:09:38 -0300

    Ordena um array de forma decrescente

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
@@ -153,11 +153,27 @@ for (let index = 0; index < myArray.length; index += 1) {
 
 // -----------------Exercício 15-----------------
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 0; index < numbers.length - 1; index += 1) {
   for (let index2 = 0; index2 < (numbers.length - index - 1); index2 += 1) {
     if (numbers[index2 + 1] < numbers[index2]) {
+      let aux = numbers[index2 + 1];
+      numbers[index2 + 1] = numbers[index2]
+      numbers[index2] = aux;
+    }
+  }
+}
+
+console.log(numbers); */
+
+// -----------------Exercício 16-----------------
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length - 1; index += 1) {
+  for (let index2 = 0; index2 < (numbers.length - index - 1); index2 += 1) {
+    if (numbers[index2 + 1] > numbers[index2]) {
       let aux = numbers[index2 + 1];
       numbers[index2 + 1] = numbers[index2]
       numbers[index2] = aux;
