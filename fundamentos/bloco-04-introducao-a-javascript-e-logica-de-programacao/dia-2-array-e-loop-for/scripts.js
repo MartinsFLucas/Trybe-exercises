08b18d94094b1a61e6493e177fb4527724a58d15
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-18 19:20:40 -0300

    Cria um array a partir do array anterior, onde cada elemento corresponde ao elemento do array anterior X o elemento seguinte) asd qqqweqweqweqweqweqwrqwrwrr
    )
    qqasd
    asd
    x

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/scripts.js
@@ -169,7 +169,7 @@ console.log(numbers); */
 
 // -----------------Exercício 16-----------------
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 0; index < numbers.length - 1; index += 1) {
   for (let index2 = 0; index2 < (numbers.length - index - 1); index2 += 1) {
@@ -181,4 +181,20 @@ for (let index = 0; index < numbers.length - 1; index += 1) {
   }
 }
 
-console.log(numbers);
+console.log(numbers); */
+
+// -----------------Exercício 17-----------------
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index + 1]) {
+    newNumbers.push(numbers[index] * numbers[index + 1]);
+  } else {
+    console.log(numbers[index + 1])
+    newNumbers.push(numbers[index] * 2)
+  }
+}
+
+console.log(newNumbers);
