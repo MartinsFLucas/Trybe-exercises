dc5e4b9d3cb58ba91546a603ac7c6911f6ed6653
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-02-21 18:08:07 -0300

    Cria um algoritmo que retorna a maior e a menor palavra dentro de um array

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/scripts.js
@@ -9,7 +9,7 @@ console.log(fatorial); */
 
 //---------------Exercício 02----------------
 
-let word = 'tryber';
+/* let word = 'tryber';
 let reversedWord = '';
 
 console.log(reversedWord);
@@ -18,4 +18,29 @@ for (let index = 0; index < word.length; index += 1) {
     reversedWord += (word[word.length - index - 1]);
 }
 
-console.log(reversedWord);
+console.log(reversedWord); */
+
+//---------------Exercício 03----------------
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let largestWord = '';
+let smallestWord = '';
+
+for (let index = 0; index < array.length; index += 1) {
+    if ((array[index]).length > largestWord.length) {
+        largestWord = array[index];
+    }
+}
+
+console.log('A maior palavra é: ', largestWord);
+
+for (let index = 0; index < array.length; index += 1) {
+    if ((array[index]).length < smallestWord.length || index === 0) {
+        smallestWord = array[index];
+    }
+}
+
+console.log('A menor palavra é: ', smallestWord);
+
+
+
