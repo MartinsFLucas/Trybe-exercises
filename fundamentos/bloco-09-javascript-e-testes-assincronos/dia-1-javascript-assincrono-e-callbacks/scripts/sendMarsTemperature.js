098afbdef177d0edd696a537c91a67a26b8414f3
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-04-20 19:10:29 -0300

    Implementa o requisito 5

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/scripts/sendMarsTemperature.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/scripts/sendMarsTemperature.js
@@ -5,8 +5,17 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-const sendMarsTemperature = () => {
-  setTimeout(() => console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`), messageDelay());
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const sendMarsTemperature = (callback) => {
+  setTimeout(() => callback(getMarsTemperature()), messageDelay());
 };
 
-sendMarsTemperature();
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
