3b31dc80e65871a3e3cf7c92ba506f1d40c769e4
Lucas Martins Figueiredo <lucas_martinsf@hotmail.com>
2022-04-21 12:44:47 -0300

    Implementa o teste solicitado no requisito 7

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/scripts/uppercase.test.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/scripts/uppercase.test.js
@@ -5,7 +5,20 @@ const uppercase = (str, callback) => {
 };
 
 describe('Teste da função uppercase', () => {
-  it('Verifica se a função uppercase converte corretamente todos os caracteres de uma string para maiúsculas', () => {
+  it('Verifica se a função uppercase converte corretamente todos os caracteres de uma string para maiúsculas', (done) => {
     expect(1).toBe(1);
+    const string = 'javascript assincrono e callbacks';
+    const expectedString = 'JAVASCRIPT ASSINCRONO E CALLBACKS';
+
+    const callback = (result) => {
+      try{
+        expect(result).toBe(expectedString)
+        done();
+      } catch(error) {
+        done(error);
+      }
+      
+    }
+    uppercase(string, callback); 
   });
 });
